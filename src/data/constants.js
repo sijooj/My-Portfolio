b9ac85a2912a4e2b813941bcda5ee343a9df9080
sijooj <sijooj73@gmail.com>
2025-01-09 17:44:40 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,119 @@
+export const Bio = {
+  name: "Sijo O.J",
+  roles: [
+    "Programmer",
+    "Frontend Developer"
+  ],
+  description:
+    "If I join a company, I can complete the work on time and give 100%.I am able to complete any task cheerfully andaccurately and on time.",
+  github: " https://github.com/sijooj",
+  resume:
+    "https://drive.google.com/file/d/10n73rB4agC8XadtIwOfX-4z2sAI5K9Bn/view",
+  linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
+  insta: "https://www.instagram.com/rishav_chanda/",
+  facebook: "https://www.facebook.com/rishav.chanda.165/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+      },
+      
+      {
+        name: "Next Js",
+      },
+      {
+        name: "HTML",
+      },
+      {
+        name: "CSS",
+      },
+      {
+        name: "JavaScript",
+      },
+      {
+        name: "Bootstrap",
+      },
+      {
+        name: "WordPress",
+      },
+      
+    ],
+  },
+ 
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+      },
+      {
+        name: "GitHub",
+      },
+      {
+        name: "VS Code",
+      },
+      {
+        name: "Postman",
+      },
+        {
+          name: "Adobe XD",
+        },
+      {
+        name: "Figma",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    
+    role: "Frontend Developer",
+    company: "CRYOFLAME TECHNOLOGIES",
+    date: "Starting Date -> 2022 April-18",
+   
+  },
+  {
+    id: 1,
+    
+    role: "Frontend Developer",
+    company: "WARMONKS SOFTWARE COMPANY",
+    date: "Starting Date -> 2024 January-2",
+   
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    school: "GOVT.R.S.R.V.H.S.S,VELUR, THRISSUR",
+    date: "2013",
+    degree: "Primary & High School",
+  },
+  {
+    id: 1,
+    school: "ASSISI ENGLISH HIGHER SECONDARY SCHOOL,THALAKKOTTUKARA",
+    date: "2017",
+    degree: "Plus Two Science",
+  },
+  {
+    id: 2,
+    school: "CHINMAYA MISSION COLLEGE, KOLAZHY",
+    date: "2020",
+    degree: "Bachelor of Computer Application(BCA)",
+  },
+  {
+    id: 3,
+    school: "LCC COMPUTER EDUCATION",
+    date: "2021",
+    degree: "Diploma in Web designing & Development",
+  }
+];
+
+
+
